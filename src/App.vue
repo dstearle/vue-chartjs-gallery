9594f22303d07e4cc08c6e43895e2c5ca46c3e75
dstearle <dallas.st.earle@gmail.com>
2019-06-14 16:14:37 -0700

    Added new styling for the title of the custom tooltips

--- src/App.vue
+++ src/App.vue
@@ -39,59 +39,5 @@
 </script>
 
 <style>
-    
-    body {
-        
-        padding: 30px;
-        
-    }
-
-    .slide-enter-active {
-
-        animation: slide-in 200ms ease-out forwards;
-
-    }
-
-    .slide-leave-active {
-
-        animation: slide-out 200ms ease-out forwards;
-
-    }
-
-    @keyframes slide-in {
-
-        from {
-
-            transform: translateY(-30px);
-            opacity: 0;
-
-        }
-
-        to {
-
-            transform: translateY(0px);
-            opacity: 1;
-
-        }
-
-    }
-
-       @keyframes slide-out {
-
-        from {
-
-            transform: translateY(0px);
-            opacity: 1;
-
-        }
-
-        to {
-
-            transform: translateY(-30px);
-            opacity: 0;
-
-        }
-
-    } 
 
 </style>
