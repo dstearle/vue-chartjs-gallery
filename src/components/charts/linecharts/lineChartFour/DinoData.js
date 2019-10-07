ed4a05ff69030b6a698ef69c172066085182c728
dstearle <dallas.st.earle@gmail.com>
2019-10-07 03:08:15 -0700

    Began work on using data from js file

---
+++ src/components/charts/linecharts/lineChartFour/DinoData.js
@@ -0,0 +1,17 @@
+export default [
+    
+    // ******* Herbivores *******
+
+        // Nasutoceratops
+        { 
+            id: 1, 
+            label: 'Nasutoceratops',
+            backgroundColor: 'rgb(0, 7, 112)',
+            borderColor: 'rgb(0, 7, 112)',
+            data: [
+                null, null, null, null, null, null, null, 
+                null, null, null, null, null, null, 20,
+            ]
+        },
+        
+];
